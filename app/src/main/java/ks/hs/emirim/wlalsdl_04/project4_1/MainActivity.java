22beb3130f34d3a94f6ec1fd363fa21144b97295
jimin <w2037@e-mirim.hs.kr>
2021-04-08 19:16:57 +0900

    Update ClickListener

--- app/src/main/java/ks/hs/emirim/wlalsdl_04/project4_1/MainActivity.java
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/project4_1/MainActivity.java
@@ -27,22 +27,22 @@ public class MainActivity extends AppCompatActivity {
         textResult = findViewById(R.id.t1);
 
 
-        btnPlus.setOnTouchListener(btnListener);
-        btnMinus.setOnTouchListener(btnListener);
-        btnMulti.setOnTouchListener(btnListener);
-        btnDiv.setOnTouchListener(btnListener);
+        btnPlus.setOnClickListener(btnListener);
+        btnMinus.setOnClickListener(btnListener);
+        btnMulti.setOnClickListener(btnListener);
+        btnDiv.setOnClickListener(btnListener);
     }
 
-    View.OnTouchListener btnListener = new View.OnTouchListener() {
+    View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
-        public boolean onTouch(View v, MotionEvent event)  {
+        public void onClick(View v) {
             String strEdit1 = edit1.getText().toString();
             String strEdit2 = edit2.getText().toString();
             int num1 = Integer.parseInt(strEdit1);
             int num2 = Integer.parseInt(strEdit2);
             int result = 0;
 
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.btn1:
                     result = num1 + num2;
                     break;
@@ -53,12 +53,13 @@ public class MainActivity extends AppCompatActivity {
                     result = num1 * num2;
                     break;
                 case R.id.btn4:
-                    result= num1 / num2;
+                    result = num1 / num2;
                     break;
             }
             textResult.setText(R.string.t1);
-            textResult.append(result +"");
-            return false;
+            textResult.append(result + "");
         }
+
     };
 }
+
